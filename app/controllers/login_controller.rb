f08de569ea70dfb69851112c5eddc90b3dabcfec
tomotin1112 <tomomi@tomotinteam8.gunma.jp>
2018-11-06 09:13:34 +0900

    修正

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -20,6 +20,7 @@ class LoginController < ApplicationController
     else
       day = member[0].register_day
       if member[0].password == Digest::MD5.hexdigest(password + day)
+        session[:user] = member[0].user_id
         return redirect_to '/top/'
       else
         flash[:error] = 'パスワードが違います'
