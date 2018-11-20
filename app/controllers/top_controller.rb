a102f4d766c01b90683c4ae7cc2d82687f9c8ddc
tomotin1112 <tomomi@tomotinteam8.gunma.jp>
2018-11-20 11:57:51 +0900

    modified

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -4,9 +4,11 @@ class TopController < ApplicationController
       @keyword = params[:keyword]
     else
       #@posts = Image.joins(:follow).where(follow_user: session[:user])
-      @posts = Image.find_by_sql(["select f.user_id,f.target_id,i.post_id,
-      i.image_id,i.image_url,count(*) as count from follow f,Image i
-      where f.target_id=i.user_id and f.user_id=?
+      @posts = Image.find_by_sql(["select f.target_id,i.post_id,
+      count(*) as count,m.account_name from follow f
+      inner join image i on f.user_id=?
+      and f.target_id=i.user_id
+      inner join member m on f.target_id=m.user_id
       group by target_id,post_id", session[:user]])
     end
   end
