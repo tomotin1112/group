5670f35d1657a31b2892b307a1dd14d167254259
tomotin1112 <tomomi@tomotinteam8.gunma.jp>
2018-11-21 20:52:17 +0900

    sql変更

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -4,10 +4,12 @@ class TopController < ApplicationController
       @keyword = params[:keyword]
     else
       @posts = Image.find_by_sql(["select f.target_id,i.post_id,
-      count(*) as count,m.account_name from follow f
+      p.title,count(*) as count,m.account_name from follow f
       inner join image i on f.user_id=?
       and f.target_id=i.user_id
       inner join member m on f.target_id=m.user_id
+      inner join post p on i.user_id=p.user_id
+      and i.post_id=p.post_id
       group by target_id,post_id", session[:user]])
     end
   end
