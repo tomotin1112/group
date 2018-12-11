0a749dbfa567983cd5c9f4671e3722dc2a5aac85
tomotin1112 <tomomi@tomotinteam8.gunma.jp>
2018-12-11 10:10:39 +0900

    fix sql

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -3,8 +3,14 @@ class TopController < ApplicationController
     if request.post?
       @keyword = params[:keyword]
     else
-      follow_user=Follow.where(user_id:session[:user])
-      @posts = Image.find_by_sql("select * from image")
+      @posts = Image.find_by_sql(["select f.target_id,i.post_id,
+      p.title,count(*) as count,m.account_name from follow f
+      inner join image i on f.user_id=?
+      and f.target_id=i.user_id
+      inner join member m on f.target_id=m.user_id
+      inner join post p on i.user_id=p.user_id
+      and i.post_id=p.post_id
+      group by target_id,post_id", session[:user]])
     end
   end
 
